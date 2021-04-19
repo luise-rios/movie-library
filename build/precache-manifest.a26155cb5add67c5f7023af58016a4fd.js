91bdc8fe7a644e81dd0bd3914e766fb74f70432c
Luise Rios <luisemiranda@gmail.com>
2021-04-19 19:33:48 -0300

    git hub pages...

---
+++ build/precache-manifest.a26155cb5add67c5f7023af58016a4fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbe387a452392a0c438f7e95ed66f3e",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "3cddf1d4331a6412f25c",
+    "url": "/movie-library/static/css/2.039bb34a.chunk.css"
+  },
+  {
+    "revision": "c39d6706a9e4825d9b23",
+    "url": "/movie-library/static/css/main.7a5a8d0e.chunk.css"
+  },
+  {
+    "revision": "3cddf1d4331a6412f25c",
+    "url": "/movie-library/static/js/2.8a5e7d35.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie-library/static/js/2.8a5e7d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39d6706a9e4825d9b23",
+    "url": "/movie-library/static/js/main.215b6109.chunk.js"
+  },
+  {
+    "revision": "a328f6845c621f999f22",
+    "url": "/movie-library/static/js/runtime-main.ec20f3b1.js"
+  }
+]);
